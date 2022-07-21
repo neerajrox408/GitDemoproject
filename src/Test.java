8d23283f922008b978be40c6b2fb6faf7f08991e
admin <admin@DESKTOP-G6DLKAJ>
2022-07-21 13:16:56 +0530

    First commit..

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+String name="neeraj";
+if(name=="neeraj")
+{
+	System.out.println("name is true");
+}
+else
+{
+	System.out.println("name is not true");
+}
+	}
+
+}
